a7fca18d9c3cbf62fb7c5b34f566f3b828e09af1
Remi Russell <remi.russell@lethpolytech.ca>
2025-11-24 14:11:10 -0700

    RatedFinale

--- src/PopularList.jsx
+++ src/PopularList.jsx
@@ -7,7 +7,7 @@ const PopularList = () => {
 
   const fetchMovies = async () => {
     const response = await fetch(
-      "https://api.themoviedb.org/3/movie/popular?api_key=55bb0acb46a61016543cb0c61a9101ab"
+      "https://api.themoviedb.org/3/movie/popular?api_key=59a23e70a835d18e0ef958c2f3194303"
     );
     ///change 'popular' to top rated, etc for diff organization
 
