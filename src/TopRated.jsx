01b008bf0d74acc09280d68c70f47fc0bcbff81b
Remi Russell <remi.russell@lethpolytech.ca>
2025-11-24 13:41:25 -0700

    Rating Test

--- src/TopRated.jsx
+++ src/TopRated.jsx
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./TopRated.css";
+import MovieCard from "./MovieCard";
 
-const TopRated = () => {
-  return <div>TopRated</div>;
+const TopRatedList = () => {
+  const [movies, setMovies] = useState([]);
+
+  const fetchMovies = async () => {
+    const response = await fetch(
+      "https://api.themoviedb.org/3/movie/popular?api_key=55bb0acb46a61016543cb0c61a9101ab"
+    );
+    ///change 'popular' to top rated, etc for diff organization
+
+    const data = await response.json();
+
+    // console.log(data);
+
+    setMovies(data.results);
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  });
+
+  return (
+    <div className="popwrap">
+      {movies.map((movie) => (
+        // console.log(movie);
+        <MovieCard key={movie.id} movie={movie} />
+      ))}
+    </div>
+  );
 };
 
 export default TopRated;
+//https://api.themoviedb.org/3/movie/popular?api_key=55bb0acb46a61016543cb0c61a9101ab
